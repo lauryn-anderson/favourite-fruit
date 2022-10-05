d2df49bf33e85f4a5777d59830c0fa741878fb28
lauryn-anderson <laurynelysse@gmail.com>
2022-10-05 15:50:30 -0600

    Initial commit

---
+++ main.py
@@ -0,0 +1,6 @@
+# A demo project for the Fall 2022 Women in Computer Science Club Git Workshop
+
+favourite_fruit = input('What is your favourite fruit?')
+
+print('Your favourite fruit is ' + favourite_fruit)
+

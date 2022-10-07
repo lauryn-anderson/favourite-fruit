a2b71af06682fedcebdd16a748ff95ef3ab5f067
lauryn-anderson <laurynelysse@gmail.com>
2022-10-06 19:54:07 -0600

    Prompt for person's name

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # A demo project for the Fall 2022 Women in Computer Science Club Git Workshop
 
 favourite_fruit = input('What is your favourite fruit?\n')
+name = input('What is your name?\n')
 
-print('Your favourite fruit is ' + favourite_fruit)
+print(name + '\'s favourite fruit is ' + favourite_fruit)
 
